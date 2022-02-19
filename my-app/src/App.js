f61ce221bc8eb793c03e0f8e52179f6dca39763b
syronc <syronc@ufl.edu>
2022-02-19 16:47:52 -0500

    Homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+// import About from  './components/about';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Home">
+      {/* <Router>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/About" element={<About/>} />
+      </Routes>
+    </Router>  */}
+      <header className="home-header">
+        <div id="navbar"></div>
+        <img src={logo} className="sesh-logo" alt="logo" />
       </header>
+      <h1>Welcome to Sesh</h1>
+      <p>Study amoung us</p>
+      <button className="btn login-register">Login/Register</button>
+      <br />
+      <button className="btn about-us">About Us</button>
+    
+    {/* <Link to="/About" >About</Link>       */}
+    
     </div>
   );
 }
