6bf6c379078dec2177e5591cb9266c8d26b11392
Cindy Ngo <62029045+cindyngo44@users.noreply.github.com>
2022-04-01 14:55:24 -0400

    profile css

--- my-app/src/components/Webpages/Profile.js
+++ my-app/src/components/Webpages/Profile.js
@@ -1,16 +1,20 @@
 import NavbarUser from '../Webpage-Components/Navbar-User';
-
+import '../Webpages/Profile.css';
 import React, { Component } from 'react'
 
 
 export default class Profile extends Component {
   render() {
     return (
-      <div>
+      <div className="background-gradient">
       <NavbarUser/>
       <br/>
       <br/>
-      <h1>Welcome {localStorage.getItem("user")} </h1>
+      <br/>
+      <br/>
+      <div className="profile">
+        <h1>Welcome, {localStorage.getItem("user")} !</h1>
+      </div>
       </div>
     )
   }
