cd32207e1c1ecb5d4140e1e287b4f314e8ba286f
syronc <syronc@ufl.edu>
2022-04-07 21:02:59 -0400

    Habit Tracker Front end

---
+++ my-app/src/components/pageComponents/Board.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Board() {
+
+const handleClick = (e) =>{
+    
+}
+
+
+  return (
+    <div>
+        <h1>Leaderboard</h1>
+
+        <div>
+            <button data-id='7' onClick={handleClick}>7 Days</button>
+            <button data-id='30' onClick={handleClick}>30 Days</button>
+            <button data-id='0' onClick={handleClick}>All Time</button>
+        </div>
+
+    </div>
+  )
+}
